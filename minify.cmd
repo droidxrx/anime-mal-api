c122693664571d76d160186e6ce52b24ed4e5b4f
DroidXrx <droidxrx@gmail.com>
2021-08-21 11:05:26 +0700

    Update

---
+++ minify.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+cd %1
+for /R %%f in (*.js) do (
+    terser %%f -o %%f  --compress --keep-classnames --keep-fnames --comments false
+)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import MAL_OAUTH2 from "./oauth2";
 import MAL_API_ANIME from "./api/anime";
+import MAL_API_MANGA from "./api/manga";
 
-if (window) window["MAL_API"] = { MAL_OAUTH2, MAL_API_ANIME };
+if (window) {
+    window["MAL_API"] = {
+        OAUTH2: MAL_OAUTH2,
+        ANIME: MAL_API_ANIME,
+        MANGA: MAL_API_MANGA,
+    };
+}
 
-export { MAL_OAUTH2, MAL_API_ANIME };
+export { MAL_OAUTH2, MAL_API_ANIME, MAL_API_MANGA };
