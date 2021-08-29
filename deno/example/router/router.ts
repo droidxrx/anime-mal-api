5dd0fd82b15fb7122de539818b73b82d07cb7b31
DroidXrX <droidxrx@gmail.com>
2021-08-30 01:58:37 +0700

    Delete Deno

--- deno/example/router/router.ts
+++
@@ -1,6 +0,0 @@
-export * from "./anime.ts";
-export * from "./manga.ts";
-export * from "./user.ts";
-export * from "./userAnimeList.ts";
-export * from "./userMangaList.ts";
-export * from "./oauth2.ts";
